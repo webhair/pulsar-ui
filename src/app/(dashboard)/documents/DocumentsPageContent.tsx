f52d21d947fc03d80f918df0ce1fccd909837f51
Matteo Budriesi <68251639+matteo2437@users.noreply.github.com>
2024-01-12 23:09:15 +0100

    add layout components

---
+++ src/app/(dashboard)/documents/DocumentsPageContent.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { TableLayout } from "@/components/TableLayout";
+
+export function DocumentsPageContent() {
+  return (
+    <TableLayout>
+      {/*qua ci va la datagrid*/}
+    </TableLayout>
+  )
+}
